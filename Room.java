105a4aa05e014f7e7b3e338f81fd078fc12e154e
rmclaurin444 <rmclaurin@ggc.edu>
2025-11-15 22:20:49 -0500

    Added a getter method for monsters

--- Room.java
+++ Room.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,13 +8,17 @@ public class Room {
     private String description;
     private boolean visited;
     private Map<String, Integer> exits;
+    private Monster monster;
+
+    private ArrayList<Item> items = new ArrayList<>();
+
 
     // Constructor
     public Room(int id, String name, String description) {
         this.id = id;
         this.name = name;
         this.description = description;
-    
+
         this.exits = new HashMap<>();
     }
 
@@ -24,9 +29,26 @@ public class Room {
     public boolean isVisited() { return visited; }
     public void setVisited(boolean visited) { this.visited = visited; }
     public void visit() { this.visited = true; }
+    public Monster getMonster() { return monster; }
+    public void setMonster(Monster monster) { this.monster = monster; }
 
     // Exits
     public void addExit(String direction, int roomNumber) { exits.put(direction.toUpperCase(), roomNumber); }
     public Map<String, Integer> getExits() { return exits; }
     public Integer getExit(String direction) { return exits.get(direction.toUpperCase()); }
+    public ArrayList<Item> getItems() { return items; }
+
+    public void addItem(Item item) {
+        items.add(item);
+    }
+
+    public Item removeItem(String name) {
+        for (Item item : items) {
+            if (item.getName().equalsIgnoreCase(name)) {
+                items.remove(item);
+                return item;
+            }
+        }
+        return null;
+    }
 }
